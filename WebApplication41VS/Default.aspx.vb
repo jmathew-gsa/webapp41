74fd2b3f7c6e7069f3f6b7dccec2eed6f05912c8
Joseph Mathew <joseph.mathew@gsa.gov>
2017-09-21 13:29:40 -0400

    Added a line for str2 String in Page_Load of Default.aspx.vb after Fetch was done.

--- WebApplication41VS/Default.aspx.vb
+++ WebApplication41VS/Default.aspx.vb
@@ -3,5 +3,7 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
 
+        Dim str2 As String = "Change2 with firsttestbranch as current branch after adding tracking to remote and doing a Fetch"
+
     End Sub
 End Class
